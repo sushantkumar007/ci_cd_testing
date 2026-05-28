18dba9146ccc0b505485e6db5f5768dedfdb0206
Sushant Kumar <sushantkumar5135@gmail.com>
2026-05-28 12:47:15 +0530

    add 5 version

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World! V4");
-})
+  res.send("Hello World! V5");
+});
 
-app.get("/health", ( req,res )=> {
-  res.send('Server is healthy')
-})
+app.get("/health", (req, res) => {
+  res.send("Server is healthy");
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
-})
+});
