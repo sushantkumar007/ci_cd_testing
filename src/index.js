a46e22ecef485bcbaf80db19949044a8b4212907
Sushant Kumar <sushantkumar5135@gmail.com>
2026-05-28 23:30:38 +0530

    add 21 version

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World! V20");
+  res.send("Hello World! V21");
 });
 
 app.get("/health", (req, res) => {
